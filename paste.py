74c0701f9bcc1b32e0d8873e6d13c35cc4d8e95a
vakansie <91331489+vakansie@users.noreply.github.com>
2024-02-14 00:22:51 +0100

    Add files via upload

--- paste.py
+++ paste.py
@@ -13,8 +13,8 @@ class UI:
         self.window = tk.Tk()
         self.window.title("Pasty Paste")
         self.create_elements()
-        self.add_button = tk.Button(self.window, text='Add Line', command= lambda: self.add_line())
-        self.refresh_button = tk.Button(self.window, text='Refresh', command= lambda: self.refresh())
+        self.add_button = tk.Button(self.window, text='Add Line', command= self.add_line)
+        self.refresh_button = tk.Button(self.window, text='Refresh', command= self.refresh)
         self.add_button.grid(row=len(self.phrases), column=0)
         self.refresh_button.grid(row=len(self.phrases), column=1)
 
@@ -22,7 +22,7 @@ class UI:
         for position, phrase in enumerate(self.phrases.keys()):
             label = tk.Label(self.window, text=phrase, font=('calibre',10, 'bold'))
             paste_button = tk.Button(self.window, text='Alt-Tab and Paste', command= paste_func_gen(phrase))
-            delete_button = tk.Button(self.window, text='Delete', command= delete_func_gen(self, phrase))
+            delete_button = tk.Button(self.window, text='Delete', command= delete_func_gen(self, self.phrases[phrase]))
             label.grid(row=position, column=0)
             paste_button.grid(row=position, column=1)
             delete_button.grid(row=position, column=2)
@@ -30,24 +30,37 @@ class UI:
     def add_line(self):
         short_description, phrase = get_new_phrase()
         if not short_description or not phrase: return
-        add_phrase_to_database(short_description, phrase)
+        database.add_phrase_to_database(short_description, phrase)
         self.refresh()
 
     def refresh(self):
         self.window.destroy()
         main()
 
+class Service:
+    def __init__(self, database) -> None:
+        self.database = database
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    def get_phrases_from_db(self) -> dict:
+        self.cursor.execute('SELECT short_description, phrase FROM Phrases')
+        rows = self.cursor.fetchall()
+        phrases = {short_desc: phrase for short_desc, phrase in rows}
+        return phrases
+
+    def add_phrase_to_database(self, short_description: str, phrase: str):
+        self.cursor.execute('INSERT INTO Phrases (short_description, phrase) VALUES (?, ?)', (short_description, phrase))
+        self.connection.commit()
+
 def get_new_phrase():
     sentence = ''
     def submit():
         nonlocal sentence
         sentence = text_entry.get("1.0", tk.END).strip()
-        if sentence:
-            messagebox.showinfo("Added:", name)
-            root.destroy()
-        else:
-            messagebox.showwarning("Warning", "Please enter a sentence.")
-            text_entry.focus_set()
+        if not sentence: return
+        messagebox.showinfo("Added:", name)
+        root.destroy()
     root = tk.Tk()
     root.withdraw()
     name = simpledialog.askstring("Input", "Enter Short Description:")
@@ -55,7 +68,7 @@ def get_new_phrase():
     text_window.title("Enter sentence to be pasted:")
     text_entry = tk.Text(text_window, height=10, width=50)
     text_entry.pack()
-    submit_button = tk.Button(text_window, text="Submit", command= lambda: submit())
+    submit_button = tk.Button(text_window, text="Submit", command= submit)
     submit_button.pack()
     root.mainloop()
     return name, sentence
@@ -65,40 +78,20 @@ def paste_func_gen(text: str) -> Callable:
         pyautogui.hotkey('alt', 'tab')
         sleep(0.1)
         pyautogui.write(message=ui.phrases[text], _pause=False)
-    return lambda: paste()
+    return paste
 
-def delete_func_gen(ui: UI, short_description: str) -> Callable:
+def delete_func_gen(ui: UI, phrase: str) -> Callable:
     def delete_row():
-        conn = sqlite3.connect(phrases_db)
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM Phrases WHERE short_description = ?', (short_description,))
-        conn.commit()
-        cursor.close()
-        conn.close()
+        database.cursor.execute('DELETE FROM Phrases WHERE phrase = ?', (phrase,))
+        database.connection.commit()
         ui.refresh()
-    return lambda: delete_row()
-
-def get_phrases_from_db() -> dict:
-    conn = sqlite3.connect(phrases_db)
-    cursor = conn.cursor()
-    cursor.execute('SELECT short_description, phrase FROM Phrases')
-    rows = cursor.fetchall()
-    phrases = {short_desc: phrase for short_desc, phrase in rows}
-    cursor.close()
-    conn.close()
-    return phrases
-
-def add_phrase_to_database(short_description: str, phrase: str):
-    conn = sqlite3.connect(phrases_db)
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO Phrases (short_description, phrase) VALUES (?, ?)', (short_description, phrase))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    return delete_row
 
 def main():
     global ui
-    phrases = get_phrases_from_db()
+    global database
+    database = Service(phrases_db)
+    phrases = database.get_phrases_from_db()
     print(phrases)
     ui = UI(phrases)
     ui.window.mainloop()
